976e14748c40f4e7b109bc89f7dda4c5c37afab9
xThrasherrr <thrashergamezz@gmail.com>
2023-08-16 21:26:04 -0600

    Update config.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,15 @@
 fx_version "adamant"
 game "rdr3"
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+lua54 'yes'
 
 description 'qr-Scoreboard'
 version '1.0.0'
 
 ui_page 'html/ui.html'
 
-shared_scripts {
-	'config.lua'
-}
-
+shared_scripts { '@ox_lib/init.lua', 'config.lua' }
 client_script 'client.lua'
 server_script 'server.lua'
 
-files {
-    'html/*'
-}
+files { 'html/*' }
